f41c6dcc2e43f6fa3779b1c47e6b2c4c57b49ffa
Mohammadbi78 <m.rezabisheh1@gmail.com>
2023-05-01 19:34:28 +0430

    add personal project

---
+++ practical_group_1402_1/personal/pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  
+  <h1>صفحه اصلی پروژه</h1>
+
+</template>
+<script>
+
+</script>
+<style>
+
+</style>
---
+++ practical_group_1402_2/personal/layouts/default.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <app_header></app_header>
+    <div class="container">
+      <Nuxt />
+    </div>
+    <app_footer></app_footer>
+  </div>
+</template>
+<script>
+import app_header from "@/components/header/header.vue";
+import app_footer from "@/components/footer/footer.vue";
+
+export default {
+  components: {
+    app_header,
+    app_footer
+  },
+};
+</script>
+<style scoped>
+.container{
+  max-width: 1500px;
+  margin: 0 auto;
+  padding: 50px;
+}
+</style>
