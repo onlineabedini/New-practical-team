3f49a830015fb257d487ddf905ca04ebe97535dc
Amir Abaspour <work.abaspour@gmail.com>
2023-05-18 19:27:11 +0330

    changed skills page and install nodejs config

--- practical_group_1402_1/personal/pages/skills.vue
+++ practical_group_1402_1/personal/pages/skills.vue
@@ -1,5 +1,79 @@
 <template>
+    <div class="container-sm" style="text-align: center;"> 
+        <div style="align-items: center; text-align: center;" class="btn-group" role="group" aria-label="Basic radio toggle button group">
+            <input type="radio" class="btn-check" name="btnradio" id="btnradio6" autocomplete="off"  checked>
+            <label class="btn btn-outline-light" for="btnradio6"style="color: black;">مهارتهای من</label>
 
+            <input type="radio" class="btn-check" name="btnradio" id="btnradio5" autocomplete="off">
+            <label class="btn btn-outline-light" for="btnradio5"style="color: black;">درباره من</label>
+            
+            <input type="radio" class="btn-check" name="btnradio" id="btnradio4" autocomplete="off">
+            <label class="btn btn-outline-light" for="btnradio4"style="color: black;">وبلاگ</label>
+            
+            <input type="radio" class="btn-check" name="btnradio" id="btnradio3" autocomplete="off">
+            <label class="btn btn-outline-light" for="btnradio3"style="color: black;">تماس با من</label>
+            
+            <input type="radio" class="btn-check" name="btnradio" id="btnradio2" autocomplete="off">
+            <label class="btn btn-outline-light" for="btnradio2" style="color: black;">نمونه کارها</label>
+
+            <input type="radio" class="btn-check" name="btnradio" id="btnradio1" autocomplete="off">
+            <label class="btn btn-outline-light" for="btnradio1"style="color: black;">خانه</label>
+        </div>
+    </div>
+    <div class="container-xxl" style="height: 200px;;">.</div>
+    <div class="container-sm">
+        <div class="container-sm" style="text-align: center;">
+            <h2>مهارتهای من</h2>
+            <p>مهارت های من در این بخش قابل نمایش است.</p>
+        </div>
+        <div class="container-sm " style="background-color: #aaaaaa;">
+            <div> </div>
+            <div style="text-align: center;">برنامه نویسی </div>
+            <div> </div>
+            <div class="progress" role="progressbar" aria-label="Basic example" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100">
+                <div class="progress-bar" style="width: 100%">HTML</div>
+              </div>
+              <div> </div>
+              <div class="progress" role="progressbar" aria-label="Basic example" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
+                <div class="progress-bar" style="width: 100%">CSS</div>
+              </div>
+              <div> </div>
+              <div class="progress" role="progressbar" aria-label="Basic example" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">
+                <div class="progress-bar" style="width: 90%">Bootstrap</div>
+              </div>
+              <div> </div>
+              <div class="progress" role="progressbar" aria-label="Basic example" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
+                <div class="progress-bar" style="width: 75%">Javascript</div>
+              </div>
+              <div> </div>
+              <div class="progress" role="progressbar" aria-label="Basic example" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100">
+                <div class="progress-bar" style="width: 80%">vue js</div>
+              </div>
+              <div> </div>
+              <div class="progress" role="progressbar" aria-label="Basic example" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100">
+                <div class="progress-bar" style="width: 70%">nodejs</div>
+              </div>
+              <div> </div>
+        </div>
+        <div> </div>
+        <div class="container-sm" style="background-color: #aaaaaa;">
+            <div> </div>
+            <div style="text-align: center;">مهارت های دیگر</div>
+            <div> </div>
+            <div class="progress" role="progressbar" aria-label="Success example" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
+                <div class="progress-bar bg-success" style="width: 50%">فن بیان</div>
+              </div>
+              <div> </div>
+              <div class="progress" role="progressbar" aria-label="Info example" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">
+                <div class="progress-bar bg-success" style="width: 35%">زبان انگلیسی</div>
+              </div>
+              <div> </div>
+              <div class="progress" role="progressbar" aria-label="Warning example" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
+                <div class="progress-bar bg-success" style="width: 40%">مدیریت اسکرام</div>
+              </div>
+              <div> </div>
+        </div>
+    </div>
 </template>
 <script>
 
