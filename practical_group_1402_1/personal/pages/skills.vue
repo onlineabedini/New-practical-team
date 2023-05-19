33b4389d91511627973ff9ac8cdfd5ba8d28f351
Amir Abaspour <work.abaspour@gmail.com>
2023-05-19 22:59:45 +0330

    Update skills.vue

--- practical_group_1402_1/personal/pages/skills.vue
+++ practical_group_1402_1/personal/pages/skills.vue
@@ -1,67 +1,64 @@
 <template>
     <div>
             
-    <div class="container-xxl" style="height: 200px;">.</div>
+    <div class="container-xxl h200" ></div>
     <div class="container-sm">
-        <div class="container-sm" style="text-align: center;">
+        <div class="container-sm txtalign">
             <h2>مهارتهای من</h2>
             <p>مهارت های من در این بخش قابل نمایش است.</p>
         </div>
-        <div class="container-sm " style="max-width: 500px;">
-            <div> </div>
-            <div style="text-align: center;">برنامه نویسی </div>
-            <div> </div>
-            <div class="progress" role="progressbar" aria-label="Basic example" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100">
-                <div class="progress-bar progress-bar-striped" style="width: 100%">HTML</div>
+        <div class="container-sm text-end mw500 ">
+            <div class="txtalign" >برنامه نویسی </div>
+            <div class="progress margin" role="progressbar" aria-label="Basic example" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100">
+                <div class="progress-bar progress-bar-striped text-end " style="width: 100%">HTML</div>
               </div>
-              <div> </div>
-              <div class="progress" role="progressbar" aria-label="Basic example" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
-                <div class="progress-bar progress-bar-striped" style="width: 100%">CSS</div>
+              <div class="progress margin" role="progressbar" aria-label="Basic example" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
+                <div class="progress-bar progress-bar-striped text-end" style="width: 100%">CSS</div>
               </div>
-              <div> </div>
-              <div class="progress" role="progressbar" aria-label="Basic example" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">
-                <div class="progress-bar progress-bar-striped" style="width: 90%">Bootstrap</div>
+              <div class="progress margin" role="progressbar" aria-label="Basic example" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">
+                <div class="progress-bar progress-bar-striped text-end" style="width: 90%">Bootstrap</div>
               </div>
-              <div> </div>
-              <div class="progress" role="progressbar" aria-label="Basic example" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
-                <div class="progress-bar progress-bar-striped" style="width: 75%">Javascript</div>
+              <div class="progress margin" role="progressbar" aria-label="Basic example" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
+                <div class="progress-bar progress-bar-striped text-end" style="width: 75%">Javascript</div>
               </div>
-              <div> </div>
-              <div class="progress" role="progressbar" aria-label="Basic example" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100">
-                <div class="progress-bar progress-bar-striped" style="width: 80%">vue js</div>
+              <div class="progress margin" role="progressbar" aria-label="Basic example" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100">
+                <div class="progress-bar progress-bar-striped text-end" style="width: 80%">vue js</div>
               </div>
-              <div> </div>
-              <div class="progress" role="progressbar" aria-label="Basic example" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100">
-                <div class="progress-bar progress-bar-striped" style="width: 70%">nodejs</div>
+              <div class="progress margin" role="progressbar" aria-label="Basic example" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100">
+                <div class="progress-bar progress-bar-striped text-end" style="width: 70%">nodejs</div>
               </div>
-              <div> </div>
         </div>
-        <div> </div>
-        <div class="container-sm" style="max-width: 500px;" >
-            <div> </div>
-            <div style="text-align: center;">مهارت های دیگر</div>
-            <div> </div>
-            <div class="progress" role="progressbar" aria-label="Success example" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
-                <div class="progress-bar bg-success progress-bar-striped" style="width: 50%">فن بیان</div>
+        <div class="container-sm text-end mw500"  >
+            <div class="txtalign" >مهارت های دیگر</div>
+            <div class="progress margin" role="progressbar" aria-label="Success example" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
+                <div class="progress-bar bg-success progress-bar-striped text-end" style="width: 50%">فن بیان</div>
               </div>
-              <div> </div>
-              <div class="progress" role="progressbar" aria-label="Info example" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">
-                <div class="progress-bar bg-success progress-bar-striped" style="width: 35%">زبان انگلیسی</div>
+              <div class="progress margin " role="progressbar" aria-label="Info example" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">
+                <div class="progress-bar bg-success progress-bar-striped text-end" style="width: 35%">زبان انگلیسی</div>
               </div>
-              <div> </div>
-              <div class="progress" role="progressbar" aria-label="Warning example" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
-                <div class="progress-bar bg-success progress-bar-striped" style="width: 40%">مدیریت اسکرام</div>
+              <div class="progress margin" role="progressbar" aria-label="Warning example" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
+                <div class="progress-bar bg-success progress-bar-striped text-end" style="width: 40%">مدیریت اسکرام</div>
               </div>
-              <div> </div>
         </div>
     </div>    
-    <div class="container-xxl" style="height: 200px;">.</div>
-    
     </div>
 </template>
 <script>
 
 </script>
 <style scoped>
+.txtalign{
+    text-align: center;
+}
+.mw500{
+    max-width: 500px;
+}
+.h200{
+    height: 200px;
+}
+.margin{
+    margin-top: 5px;
+    margin-bottom: 5px;
+}
 
 </style>
