b6bae77e126115ab1cc262371c89983171f05799
Amir Abaspour <work.abaspour@gmail.com>
2023-05-19 12:44:13 +0330

    Update skills.vue

--- practical_group_1402_1/personal/pages/skills.vue
+++ practical_group_1402_1/personal/pages/skills.vue
@@ -1,25 +1,5 @@
 <template>
-    <div class="container-sm" style="text-align: center;"> 
-        <div style="align-items: center; text-align: center;" class="btn-group" role="group" aria-label="Basic radio toggle button group">
-            <input type="radio" class="btn-check" name="btnradio" id="btnradio6" autocomplete="off"  checked>
-            <label class="btn btn-outline-light" for="btnradio6"style="color: black;">مهارتهای من</label>
-
-            <input type="radio" class="btn-check" name="btnradio" id="btnradio5" autocomplete="off">
-            <label class="btn btn-outline-light" for="btnradio5"style="color: black;">درباره من</label>
-            
-            <input type="radio" class="btn-check" name="btnradio" id="btnradio4" autocomplete="off">
-            <label class="btn btn-outline-light" for="btnradio4"style="color: black;">وبلاگ</label>
-            
-            <input type="radio" class="btn-check" name="btnradio" id="btnradio3" autocomplete="off">
-            <label class="btn btn-outline-light" for="btnradio3"style="color: black;">تماس با من</label>
-            
-            <input type="radio" class="btn-check" name="btnradio" id="btnradio2" autocomplete="off">
-            <label class="btn btn-outline-light" for="btnradio2" style="color: black;">نمونه کارها</label>
-
-            <input type="radio" class="btn-check" name="btnradio" id="btnradio1" autocomplete="off">
-            <label class="btn btn-outline-light" for="btnradio1"style="color: black;">خانه</label>
-        </div>
-    </div>
+    
     <div class="container-xxl" style="height: 200px;;">.</div>
     <div class="container-sm">
         <div class="container-sm" style="text-align: center;">
