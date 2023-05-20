58b953eb8568321dedd39a7f69286c94332f95cd
Msdr84 <131704194+Msdr84@users.noreply.github.com>
2023-05-20 06:48:15 -0700

    header.vue

--- practical_group_1402_1/personal/components/header/header.vue
+++ practical_group_1402_1/personal/components/header/header.vue
@@ -1,14 +1,40 @@
 <template>
-    <!-- html -->
-  
-  </template>
-  <script>
-  
-  //this is for java script
-  
-  </script>
-  <style scoped>
-  
-  /* this is for css */
-  
-  </style>
+  <div>
+    <div class="border text-center  p-5 mb-5   rounded-5">
+      <h1 class=" title ">
+        <p>وبسایت شخصی مطالعاتی</p>
+      </h1>
+      <hr>
+      <!--Add menus-->
+      <div class="p-1">
+        <p>
+          <a class="link-dark  text-decoration-none fs-5 ms-4" href="/homme">
+          خانه
+          </a>
+          <a class="link-dark  text-decoration-none fs-5 ms-4" href="/samples">
+          نمونه کارها
+          </a>
+          <a class="link-dark  text-decoration-none fs-5 ms-4" href="/contact">
+          تماس با من
+          </a>
+          <a class="link-dark  text-decoration-none fs-5 ms-4" href="/blog">
+          وبلاگ
+          </a>
+          <a class="link-dark  text-decoration-none fs-5 ms-4" href="/about">
+          درباره من
+          </a>
+          <a class="link-dark  text-decoration-none fs-5 ms-4" href="/skills">
+          مهارت های من
+          </a>
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+<style>
+/*Add a shadow to the element*/
+.border {
+  box-shadow: 2px 30px 20px 2px rgba(0, 0, 0, 0.3);
+}
+</style>
+
