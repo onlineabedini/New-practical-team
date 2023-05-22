7a8a575104cdfc5dce8d18dced98982d9ddc2fc7
DrZxa <Sinasaberi90@gmail.com>
2023-05-22 19:58:24 +0330

    comment added index.vue

--- practical_group_1402_1/personal/pages/index.vue
+++ practical_group_1402_1/personal/pages/index.vue
@@ -2,11 +2,12 @@
   <div>
     <div class="row">
       <div class="col-8">
-        <div class="text-start w-76  bd_shadow rounded">
+        <div class="text-start w-76 bd_shadow rounded">
           <p>مهارت های من</p>
         </div>
-
         <p class="p-2" style="margin-top: 10px;"> برنامه نویسی</p>
+
+        <!-- progress bar for  مهارت های من  -->
 
         <div class="container">
           <div div class="mx-auto">
@@ -23,6 +24,8 @@
         </div>
 
         <p class="p-2" style="margin-top: 10px;">مهارت های دیگر </p>
+
+        <!-- progress bar for  مهارت های دیگر -->
 
         <div class="container">
           <div class="mx-auto">
@@ -47,6 +50,9 @@
         </div>
 
         <div class="text-start w-76 bd_shadow rounded mt-5">
+
+          <!-- about me section -->
+
           <p>درباره من</p>
         </div>
         <p class="p-2 text-break w-76">
@@ -59,11 +65,16 @@
         </p>
 
         <div class="text-start w-76 bd_shadow rounded">
+
+          <!-- blog post section -->
+
           <p>پست های وبلاگ</p>
         </div>
         <p class="p-2"> test</p>
       </div>
       <div class="col-4">
+
+        <!-- picture section -->
 
         <div class="row row-cols-2 bd_shadow rounded ps-3">
           <div class="col position-relative">
@@ -92,9 +103,10 @@
 
   </div>
 </template>
-<script>
 
+<script>
 </script>
+
 <style>
 .bd_shadow {
   box-shadow: 0px 0px 5px 1px rgb(81, 149, 158);
