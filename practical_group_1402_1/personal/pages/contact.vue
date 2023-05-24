a0e9a61864b9e0ff8966f3837e47c6ec7e10fed3
Msdr84 <131704194+Msdr84@users.noreply.github.com>
2023-05-24 00:44:11 -0700

    contact.vue
    I did it

--- practical_group_1402_1/personal/pages/contact.vue
+++ practical_group_1402_1/personal/pages/contact.vue
@@ -1,14 +1,27 @@
 <template>
-    <!-- html -->
-  
-  </template>
-  <script>
-  
-  //this is for java script
-  
-  </script>
-  <style scoped>
-  
-  /* this is for css */
-  
-  </style>
+  <div>
+      <h1 class="text-center mb-5">
+        ارتباط با من
+        <!--textboxes and textarea-->
+          <div class="rounded-pill  p-5 mb-5">
+            <div class="text">
+             <input type="text" id="inputname" placeholder="نام و نام خانوادگی خود را وارد کنید" class="form-control mb-3 w-50">
+             <input type="text" id="inputemail" placeholder="ایمیل خود را وارد کنید" class="form-control mb-3 w-50">
+             <textarea class="form-control mb-3 w-50" id="Textarea" placeholder="پیام خود را در این بخش وارد کنید"></textarea>
+            </div>
+             <button type="button" class="btn btn-primary">ارسال</button>
+          </div>
+      </h1>
+  </div>
+</template>
+
+
+<style>
+/*Bring in the middle textboxes and textarea*/
+.text{
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+}
+</style>
