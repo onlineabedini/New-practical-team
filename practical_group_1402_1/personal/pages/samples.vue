c7390488c1c42822dad4d92d8000b5723bd7cfd1
zahra <zhralran63@gmail.com>
2023-05-24 18:17:55 +0430

    this is my code

--- practical_group_1402_1/personal/pages/samples.vue
+++ practical_group_1402_1/personal/pages/samples.vue
@@ -1,4 +1,5 @@
 <template>
+<<<<<<< Updated upstream
     <!-- html -->
   
   </template>
@@ -12,3 +13,66 @@
   /* this is for css */
   
   </style>
+=======
+  <div v-for="card in sample_card" :key="card.id">
+    <h1>
+      {{ card.title }}
+    </h1>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      sample_card: {
+        sample1: {
+          id: 1,
+          img_url:
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-QYyKqnWL_KjJFEVbzeafD1fOgUAWGdqNhA&usqp=CAU",
+          title: "نمونه کار 1",
+          description:
+            "این یک نمونه کار آزمایشی از مالک وبسایت است که از سمت سرور برای شما ارسال شده.",
+          data: `
+        این  محتوای یک نمونه کار است که برای شما ارسال شده است تا بتوانید به درستی کار خود را توسعه دهید و در فرآیند یادگیری برنامه نویسی موفق شوید
+        `,
+        },
+        sample2: {
+          id: 1,
+          img_url:
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-QYyKqnWL_KjJFEVbzeafD1fOgUAWGdqNhA&usqp=CAU",
+          title: "نمونه کار 1",
+          description:
+            "این یک نمونه کار آزمایشی از مالک وبسایت است که از سمت سرور برای شما ارسال شده.",
+          data: `
+        این  محتوای یک نمونه کار است که برای شما ارسال شده است تا بتوانید به درستی کار خود را توسعه دهید و در فرآیند یادگیری برنامه نویسی موفق شوید
+        `,
+        },
+        sample3: {
+          id: 1,
+          img_url:
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-QYyKqnWL_KjJFEVbzeafD1fOgUAWGdqNhA&usqp=CAU",
+          title: "نمونه کار 1",
+          description:
+            "این یک نمونه کار آزمایشی از مالک وبسایت است که از سمت سرور برای شما ارسال شده.",
+          data: `
+        این  محتوای یک نمونه کار است که برای شما ارسال شده است تا بتوانید به درستی کار خود را توسعه دهید و در فرآیند یادگیری برنامه نویسی موفق شوید
+        `,
+        },
+        sample4: {
+          id: 1,
+          img_url:
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-QYyKqnWL_KjJFEVbzeafD1fOgUAWGdqNhA&usqp=CAU",
+          title: "نمونه کار 1",
+          description:
+            "این یک نمونه کار آزمایشی از مالک وبسایت است که از سمت سرور برای شما ارسال شده.",
+          data: `
+        این  محتوای یک نمونه کار است که برای شما ارسال شده است تا بتوانید به درستی کار خود را توسعه دهید و در فرآیند یادگیری برنامه نویسی موفق شوید
+        `,
+        },
+      },
+    };
+  },
+};
+</script>
+<style scoped></style>
+>>>>>>> Stashed changes
