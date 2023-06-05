1973fb32ec3cacbb9f5c089332b8ee3f99177fe6
Msdr84 <131704194+Msdr84@users.noreply.github.com>
2023-06-05 02:13:53 -0700

    id.vue

--- practical_group_1402_1/personal/pages/article/_id.vue
+++ practical_group_1402_1/personal/pages/article/_id.vue
@@ -2,31 +2,47 @@
   <!-- html -->
   <div>
     <div class="text-center h2">
-      مقاله 1
+      {{ one_article.title }}
     </div>
     <div class="text-center h4">
-      این یک مقاله ازمایشی است که از سمت سرور برای شما ارسال شده است
+      {{ one_article.description }}
     </div>
     <div class="text-center">
       <div>
         <!-- Photo section  -->
-        <img src="@/static/img/3.jpg" alt="Image" class="w-75">
-      </div>
-      <br><br>
-      <div class="h5 pt-5">
-        این محتوای مقاله ازمایشی است که از سمت سرور برای مطالعه کاربر برنامه نویسی مثل شما ارسال شده است ،امیدواریم که در
-        فرایند توسعه موفق باشید
+        <img :src=" one_article.img_url" alt="Image" class="w-75" >
       </div>
     </div>
+      <br><br>
+    <div class="h5 pt-5">
+         {{ one_article.content }}
+    </div> 
   </div>
 </template>
-<script>
 
+<script>
 //this is for java script
 export default {
-  layout: 'pages'
+  layout: 'pages',
+  data() {
+    return{
+      one_article:[],
+      id:1
+    }
+  },
+  mounted(){
+    this.id=this.$route.params.id
+     this.$axios.get('http://localhost:8080/api/article/' + this.id)
+    .then(response => {
+        this.one_article = response.data
+        console.log(this.one_article)
+      })
+      .catch(error => { console.log(error) })
+
+  }
 }
 </script>
+
 <style scoped>
 /* this is for css */
 </style>
